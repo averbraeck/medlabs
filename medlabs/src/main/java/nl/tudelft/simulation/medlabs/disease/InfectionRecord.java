990e32d470d67bb51a3b97d44906772447e120e9
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-05-20 09:03:56 +0200

    Issue #6. Add InfectionRecord for results of infections
    This is used to calculate statistics and offspring

---
+++ medlabs/src/main/java/nl/tudelft/simulation/medlabs/disease/InfectionRecord.java
@@ -0,0 +1,99 @@
+package nl.tudelft.simulation.medlabs.disease;
+
+import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TIntArrayList;
+
+/**
+ * InfectionRecord contains the result of an exposure of uninfected persons to infectious persons over a duration.
+ * <p>
+ * Copyright (c) 2024-2024 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. The
+ * MEDLABS project (Modeling Epidemic Disease with Large-scale Agent-Based Simulation) is aimed at providing policy analysis
+ * tools to predict and help contain the spread of epidemics. It makes use of the DSOL simulation engine and the agent-based
+ * modeling formalism. See for project information <a href="http://www.simulation.tudelft.nl/"> www.simulation.tudelft.nl</a>.
+ * The original MEDLABS Java library was developed as part of the PhD research of Mingxin Zhang at TU Delft and is described in
+ * the PhD thesis "Large-Scale Agent-Based Social Simulation" (2016). This software is licensed under the BSD license. See
+ * license.txt in the main project.
+ * </p>
+ * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ */
+public class InfectionRecord
+{
+    /**
+     * Construct an InfectionRecord.
+     * @param exposedPhase DiseasePhase; the disease phase that the person will get after 'successful' exposure
+     */
+    public InfectionRecord(final DiseasePhase exposedPhase)
+    {
+        super();
+        this.exposedPhase = exposedPhase;
+    }
+
+    /** The disease phase associated with the state that the person will get after 'successful' exposure. */
+    private final DiseasePhase exposedPhase;
+
+    /** The list of infectious persons currently in the (sub)location. */
+    private TIntList infectiousPersons = new TIntArrayList();
+
+    /** The list of infected persons in the (sub)location as a result of the exposure. */
+    private TIntList infectedPersons = new TIntArrayList();
+
+    /** Whether a transmission calculation was carried out or not. */
+    private boolean calculated = false;
+
+    /**
+     * @return infectiousPersons
+     */
+    public TIntList getInfectiousPersons()
+    {
+        return this.infectiousPersons;
+    }
+
+    /**
+     * @param infectiousPerson add this infectious person to the list
+     */
+    public void addInfectiousPerson(final int infectiousPerson)
+    {
+        this.infectiousPersons.add(infectiousPerson);
+    }
+
+    /**
+     * @return infectedPersons
+     */
+    public TIntList getInfectedPersons()
+    {
+        return this.infectedPersons;
+    }
+
+    /**
+     * @param infectedPerson add this infectious person to the list
+     */
+    public void addInfectedPerson(final int infectedPerson)
+    {
+        this.infectedPersons.add(infectedPerson);
+    }
+
+    /**
+     * @return calculated
+     */
+    public boolean isCalculated()
+    {
+        return this.calculated;
+    }
+
+    /**
+     * @param calculated set calculated
+     */
+    public void setCalculated(final boolean calculated)
+    {
+        this.calculated = calculated;
+    }
+
+    /**
+     * @return exposedPhase
+     */
+    public DiseasePhase getExposedPhase()
+    {
+        return this.exposedPhase;
+    }
+
+}
