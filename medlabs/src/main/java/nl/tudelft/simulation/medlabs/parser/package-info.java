7a3488ea8cda5fbdaa17e1e05294d5f047186bb8
Alexander Verbraeck <a.verbraeck@gmail.com>
2025-10-30 09:57:27 +0100

    Remove old sub-project structure (simplify)

--- medlabs/src/main/java/nl/tudelft/simulation/medlabs/parser/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * The parser package has parsers for more complex properties (distributions, tables). 
- */
-package nl.tudelft.simulation.medlabs.parser;
--- medlabs/src/main/java/nl/tudelft/simulation/medlabs/simulation/SimpleAnimatorInterface.java
+++
@@ -1,8 +0,0 @@
-package nl.tudelft.simulation.medlabs.simulation;
-
-import nl.tudelft.simulation.dsol.simulators.AnimatorInterface;
-
-public interface SimpleAnimatorInterface extends AnimatorInterface
-{
-
-}
